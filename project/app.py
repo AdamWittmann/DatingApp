81c67c3c42f889b3c2c3df87d931ac7c2c2f8994
PedroGuitian <143512593+PedroGuitian@users.noreply.github.com>
2024-10-22 18:39:19 -0400

    changed the name to templates for HTML files and worked on app.py

--- project/app.py
+++ project/app.py
@@ -6,7 +6,23 @@ from db.server import app
 # create a webpage based off of the html in templates/index.html
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route('/signup')
+def index():
+    return render_template("signup.html")
+
+@app.route('/filtr')
+def index():
+    return render_template("filtr.html")
+
+@app.route('/messages')
+def index():
+    return render_template("messages.html")
+
+@app.route('/profile')
+def index():
+    return render_template("profile.html")
 
 if __name__ == "__main__":
     # debug refreshes your application with your new changes every time you save
